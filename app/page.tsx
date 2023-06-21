05b6856f5974673639efc04e57fa83e22aa610b3
Henry <hnguyhenry@protonmail.com>
2023-06-21 00:06:55 -0700

    Add dependencies for leaflet

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 import Image from 'next/image';
+import dynamic from 'next/dynamic';
 
 export default function Home() {
-  return <main className=''></main>;
+  const Map = dynamic(() => import('../components/LeafMap'),
+    {
+      loading: () => <p>A map is loading..</p>,
+      ssr: false}
+  )
+
+  return (
+    <main className=''>
+      <div className=''>
+        <Map />
+      </div>
+    </main>
+  );
 }
---
+++ components/LeafMap.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+// REFERENCE: https://stackoverflow.com/a/64634759
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
+import 'leaflet-defaulticon-compatibility';
+
+const Map = () => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: 400, width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
