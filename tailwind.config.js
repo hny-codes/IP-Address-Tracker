e82a0e23b410e411f4994e29b8983be027a850be
Henry <hnguyhenry@protonmail.com>
2023-06-20 22:46:12 -0700

    Add global styles from style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        rubik: ['var(--font-rubik)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +18,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
