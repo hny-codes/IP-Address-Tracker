63cdb97f74a6e34f63c8e908c8847301645de3e3
Henry <hnguyhenry@protonmail.com>
2023-06-22 16:20:04 -0700

    Update component styling

--- app/components/InputSection.tsx
+++ app/components/InputSection.tsx
@@ -31,50 +31,51 @@ export default function InputSection({
     <section
       className={`relative bg-[url('/images/pattern-bg-mobile.png')] bg-cover py-6 z-10 font-rubik`}
     >
-      <div className='relative w-[90%] mx-auto flex flex-col items-center'>
-        <h1 className='text-3xl text-white font-bold mb-8'>
+      <div className='relative w-[85%] mx-auto flex flex-col items-center'>
+        <h1 className='text-2xl text-white font-bold mb-5'>
           IP Address Tracker
         </h1>
         <div className='mb-28 flex w-full'>
           <input
-            className='w-full py-4 px-6 rounded-xl rounded-r-none'
+            className='w-full py-3 px-6 rounded-xl rounded-r-none'
             type='text'
             value={value}
             onChange={handleChange}
+            placeholder='Search for any IP address or domain'
           />
           <button
-            className='py-4 px-6 bg-black text-white font-bold rounded-xl rounded-l-none'
+            className='py-3 px-6 bg-black text-white font-bold rounded-xl rounded-l-none'
             onClick={() => setIp(value)}
           >
             {'>'}
           </button>
         </div>
-        <div className='flex flex-col justify-evenly gap-6 p-4 text-center bg-white w-full border rounded-xl shadow-md absolute top-36 '>
+        <div className='flex flex-col justify-evenly gap-5 p-4 text-center bg-white w-full border rounded-xl shadow-md absolute top-[7.5rem] '>
           <div>
-            <h3 className='uppercase text-dark-gray font-[700] text-[0.7rem]'>
+            <h3 className='uppercase text-dark-gray font-[700] text-[0.6rem]'>
               ip address
             </h3>
-            <p className='font-bold text-xl'>{ip}</p>
+            <p className='font-bold text-lg'>{ip}</p>
           </div>
           <div>
-            <h3 className='uppercase text-dark-gray font-[700] text-[0.7rem]'>
+            <h3 className='uppercase text-dark-gray font-[700] text-[0.6rem]'>
               location
             </h3>
-            <p className='font-bold text-xl'>
+            <p className='font-bold text-lg'>
               {city}, {region} {postalCode}
             </p>
           </div>
           <div>
-            <h3 className='uppercase text-dark-gray font-[700] text-[0.7rem]'>
+            <h3 className='uppercase text-dark-gray font-[700] text-[0.6rem]'>
               timezone
             </h3>
-            <p className='font-bold text-xl'>UTC {timezone}</p>
+            <p className='font-bold text-lg'>UTC {timezone}</p>
           </div>
           <div>
-            <h3 className='uppercase text-dark-gray font-[700] text-[0.7rem]'>
+            <h3 className='uppercase text-dark-gray font-[700] text-[0.6rem]'>
               isp
             </h3>
-            <p className='font-bold text-xl'>{isp}</p>
+            <p className='font-bold text-lg'>{isp}</p>
           </div>
         </div>
       </div>
