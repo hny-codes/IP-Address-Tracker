15b2a12da5ddd0a360ce0fd8d6b37db978431f05
Henry <hnguyhenry@protonmail.com>
2023-06-22 19:30:54 -0700

    Change marker icon with svg

--- components/LeafMap.tsx
+++ components/LeafMap.tsx
@@ -5,6 +5,13 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
+import L, { Point } from 'leaflet';
+
+const customIcon = new L.Icon({
+  iconUrl: '/images/icon-location.svg',
+  iconSize: [40, 45],
+  className: 'custom-icon',
+});
 
 type MapCoord = {
   lat: number;
@@ -23,7 +30,7 @@ const Map = ({ lat, lng }: MapCoord) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={[lat, lng]}>
+      <Marker position={[lat, lng]} icon={customIcon}>
         <Popup>
           Your Coordinates: <br />
           {lat} | {lng}
