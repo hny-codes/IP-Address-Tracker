260b3565e49bac4eafbb8cc8f7fb7484eb061583
Henry <hnguyhenry@protonmail.com>
2023-06-22 21:22:49 -0700

    Remove div

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import App from '@/components/App';
 
 export default function Home() {
   return (
-    <main className=''>
-      <div className='relative'>
-        <App />
-      </div>
+    <main className='relative'>
+      <App />
     </main>
   );
 }
