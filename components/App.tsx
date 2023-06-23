f87b41074e5ab1ecec8a70ba129f3a8769e7c869
Henry <hnguyhenry@protonmail.com>
2023-06-23 14:49:25 -0700

    Add spinner svg to loader component

--- components/App.tsx
+++ components/App.tsx
@@ -7,9 +7,40 @@ import InputSection from '@/app/components/InputSection';
 
 const Loading = () => {
   return (
-    <>
+    <div className='flex flex-col items-center'>
       <p className='font-rubik font-bold'>A map is loading..</p>
-    </>
+      <svg
+        width='75'
+        height='75'
+        viewBox='0 0 75 75'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+        className='animate-spin'
+      >
+        <circle
+          cx='38'
+          cy='38'
+          r='23'
+          fill='white'
+          stroke='#2B2B2B'
+          stroke-width='5'
+        />
+        <mask
+          id='mask0_1_5'
+          style={{ maskType: 'alpha' }}
+          maskUnits='userSpaceOnUse'
+          x='13'
+          y='13'
+          width='50'
+          height='50'
+        >
+          <circle cx='38' cy='38' r='23' stroke='#2B2B2B' stroke-width='5' />
+        </mask>
+        <g mask='url(#mask0_1_5)'>
+          <path d='M36 13H63V40H36V13Z' fill='#969696' />
+        </g>
+      </svg>
+    </div>
   );
 };
 
